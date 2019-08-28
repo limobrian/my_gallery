e2e3d31eaca036972ac10dd4e72757f0b3e130fb
limooh <limobrian290@gmail.com>
2019-08-28 11:11:44 +0300

    added include from django .conf. urls module  && allows to  refere  to other URLconf

--- photos/urls.py
+++ photos/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.photos_of_day,name='photosToday')
-
+    # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
+
+
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+
+def photos_of_day(request):
+    date = dt.date.today()
+    photos = Image.photos_of_day()
+    return render(request, 'all/today_photos.html', {"date": date, "photos":photos})
    
    
    
